56ac9a83bae425b1a502ae01491043d16d8ad518
Richard Almonte <richard_almonte@hotmail.com>
2023-10-01 19:00:06 +0200

    Remove old blog post, add new blog posts and Category related components
    Old blog post was removed due to its irrelevance. Six new blog posts have been added with updated content. To better classify these posts, additional components related to Categories have been created. Changes in respective pages and modifications in the image paths were necessary to align with the changes. New styles were added in Tailwind config for typographies and introduced '@astrojs/mdx' for handling Markdown files.
    
    The changes were designed to enhance the user interface, improve blog navigation and add fresh content to the website. The additional categories feature provides the users a more filtered search experience. The style updates improve the overall visual appeal of the site.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,25 @@
 import {defineCollection, z} from 'astro:content';
 
+const authorsCollection = defineCollection({
+    schema: ({image}) =>
+        z.object({
+            name: z.string(),
+            image: image(),
+        }),
+})
+
 const postsCollection = defineCollection({
-    schema: z.object({
-        author: z.string(),
-        date: z.string(),
-        image: z.string(),
-        title: z.string(),
-    }),
+    schema: ({image}) =>
+        z.object({
+            author: z.string(),
+            categories: z.array(z.string()),
+            date: z.string(),
+            image: image(),
+            title: z.string(),
+        }),
 });
 
 export const collections = {
+    authors: authorsCollection,
     posts: postsCollection,
 };
