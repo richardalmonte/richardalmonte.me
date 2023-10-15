31ac8c52a6d396eccf5d75d80155e9d88d5b52be
Richard Almonte <richard_almonte@hotmail.com>
2023-10-15 18:48:52 +0200

    feat(env): Add bun support
    Replaces npm with bun.
    - Modify Github Action.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
+import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [mdx(), sitemap(), tailwind({
+    configFile: './tailwind.config.cjs'
+  })]
 });
